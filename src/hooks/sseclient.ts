56f10185b612d02065f1eb0f500a4b006ae5dac3
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2023-12-30 06:05:29 +0800

    feat: folder layout has been initialized and sse listener for receiving files list has been added and functional with parsing compatibility

--- src/hooks/sseclient.ts
+++ src/hooks/sseclient.ts
@@ -2,7 +2,7 @@ import sign from 'jwt-encode'
 import { Dispatch } from 'redux';
 import { ActionProp, AuthStateInterface } from './interfaces';
 import jwt_decode from 'jwt-decode'
-import { SET_COUNTER_ON_SSE_OPEN, SET_DEVICE_INFO, SET_DEVICE_LIST } from '@/redux/types';
+import { SET_COUNTER_ON_SSE_OPEN, SET_DEVICE_DIRNPATH, SET_DEVICE_INFO, SET_DEVICE_LIST } from '@/redux/types';
 
 const API = import.meta.env.VITE_NEON_AI_API;
 const SECRET = import.meta.env.VITE_JWT_SECRET;
@@ -78,6 +78,43 @@ const SSENotificationsTRequest = (authentication: AuthStateInterface, dispatch: 
                 type: SET_DEVICE_INFO,
                 payload: {
                     deviceinfo: decodedResult.data
+                }
+            })
+        }
+    })
+
+    sseNtfsSource.addEventListener('devicefileslist', (e) => {
+        const parsedresponse = JSON.parse(e.data)
+        if(parsedresponse.status){
+            const decodedResult: any = jwt_decode(parsedresponse.result)
+
+            //play ringtone
+            
+            //execute notification handling
+            // console.log(decodedResult.data);
+
+            const devicefileslistdata = [
+                ...decodedResult.data.dirs.map((mp: any) => ({
+                    filename: decodeURIComponent(mp).split("\\")[decodeURIComponent(mp).split("\\").length - 1],
+                    path: mp,
+                    type: "folder"
+                })),
+                ...decodedResult.data.files.map((mp: any) => ({
+                    filename:  decodeURIComponent(mp).split("\\")[decodeURIComponent(mp).split("\\").length - 1],
+                    path: mp,
+                    type: "file"
+                }))
+            ]
+
+            // console.log(devicefileslistdata, decodedResult.data.path);
+
+            dispatch({
+                type: SET_DEVICE_DIRNPATH,
+                payload: {
+                    dirnpath: {
+                        directory: decodedResult.data.path,
+                        list: devicefileslistdata,
+                    }
                 }
             })
         }
