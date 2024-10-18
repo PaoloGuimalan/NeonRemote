a6eab6d2378f9e588ed87464f667e5e950bdc64d
Paolo Guimalan <pauloportes.guimalan187@gmail.com>
2024-10-18 20:39:36 +0800

    feat: file fetch request is now working but not contained

--- src/hooks/endpoints.ts
+++ src/hooks/endpoints.ts
@@ -1,22 +1,20 @@
 const AUTH = {
-    login: '/auth/login',
-    register: '/auth/register',
-    refreshauth: '/auth/refreshauth',
-    verification: '/auth/verification'
-}
+  login: "/auth/login",
+  register: "/auth/register",
+  refreshauth: "/auth/refreshauth",
+  verification: "/auth/verification",
+};
 
 const GET = {
-    getdevices: '/access/getdevices',
-    getdeviceinfo: '/access/getdeviceinfo/',
-    getdevicefiles: '/access/getdevicefiles/'
-}
+  getdevices: "/access/getdevices",
+  getdeviceinfo: "/access/getdeviceinfo/",
+  getdevicefiles: "/access/getdevicefiles/",
+};
 
 const POST = {
-    adddevice: '/access/adddevice'
-}
+  adddevice: "/access/adddevice",
+  fetchfile: "/access/fetchfile",
+};
 
-export {
-    AUTH,
-    GET,
-    POST
-}
+export { AUTH, GET, POST };
+
--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,71 +1,91 @@
-import { ActionProp, FetchedDeviceDataInterface, SystemLogsItem } from "@/hooks/interfaces";
-import { SET_AUTHENTICATION, SET_COUNTER_ON_SSE_OPEN, SET_DEVICE_DIRECTORY, SET_DEVICE_DIRNPATH, SET_DEVICE_INFO, SET_DEVICE_LIST, SET_SYSTEM_LOGS } from "../types";
+/* eslint-disable no-case-declarations */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  ActionProp,
+  FetchedDeviceDataInterface,
+  SystemLogsItem,
+} from "@/hooks/interfaces";
+import {
+  SET_AUTHENTICATION,
+  SET_COUNTER_ON_SSE_OPEN,
+  SET_DEVICE_DIRECTORY,
+  SET_DEVICE_DIRNPATH,
+  SET_DEVICE_INFO,
+  SET_DEVICE_LIST,
+  SET_SYSTEM_LOGS,
+} from "../types";
 import { authenticationstate, fetchedDeviceDataState } from "./states";
 
-export const setauthentication = (state = authenticationstate, action: ActionProp) => {
-    switch(action.type){
-        case SET_AUTHENTICATION:
-            return action.payload.authentication;
-        default:
-            return state;
-    }
-}
+export const setauthentication = (
+  state = authenticationstate,
+  action: ActionProp
+) => {
+  switch (action.type) {
+    case SET_AUTHENTICATION:
+      return action.payload.authentication;
+    default:
+      return state;
+  }
+};
 
 export const setcounteronsseopen = (state: number = 0, action: ActionProp) => {
-    switch(action.type){
-        case SET_COUNTER_ON_SSE_OPEN:
-            const newstate = state + 1;
-            return newstate;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case SET_COUNTER_ON_SSE_OPEN:
+      const newstate = state + 1;
+      return newstate;
+    default:
+      return state;
+  }
+};
 
-export const setdevicelist = (state : any[] = [], action: ActionProp) => {
-    switch(action.type){
-        case SET_DEVICE_LIST:
-            return action.payload.devicelist;
-        default:
-            return state;
-    }
-}
+export const setdevicelist = (state: any[] = [], action: ActionProp) => {
+  switch (action.type) {
+    case SET_DEVICE_LIST:
+      return action.payload.devicelist;
+    default:
+      return state;
+  }
+};
 
-export const setdeviceinfo = (state : FetchedDeviceDataInterface = fetchedDeviceDataState, action: ActionProp) => {
-    switch(action.type){
-        case SET_DEVICE_INFO:
-            return {
-                ...state,
-                ...action.payload.deviceinfo
-            };
-        case SET_DEVICE_DIRECTORY:
-            return {
-                ...state,
-                files: {
-                    ...state.files,
-                    directory: action.payload.directorypath
-                }
-            }
-        case SET_DEVICE_DIRNPATH:
-            return {
-                ...state,
-                files: {
-                    ...state.files,
-                    ...action.payload.dirnpath
-                }
-            }
-        default:
-            return state;
-    }
-}
+export const setdeviceinfo = (
+  state: FetchedDeviceDataInterface = fetchedDeviceDataState,
+  action: ActionProp
+) => {
+  switch (action.type) {
+    case SET_DEVICE_INFO:
+      return {
+        ...state,
+        ...action.payload.deviceinfo,
+      };
+    case SET_DEVICE_DIRECTORY:
+      return {
+        ...state,
+        files: {
+          ...state.files,
+          directory: action.payload.directorypath,
+        },
+      };
+    case SET_DEVICE_DIRNPATH:
+      return {
+        ...state,
+        files: {
+          ...state.files,
+          ...action.payload.dirnpath,
+        },
+      };
+    default:
+      return state;
+  }
+};
 
-export const setsystemlogs = (state: SystemLogsItem[] = [], action: ActionProp) => {
-    switch(action.type){
-        case SET_SYSTEM_LOGS:
-            return [
-                action.payload.newlog,
-                ...state.reverse()
-            ];
-        default:
-            return state.reverse();
-    }
-}
+export const setsystemlogs = (
+  state: SystemLogsItem[] = [],
+  action: ActionProp
+) => {
+  switch (action.type) {
+    case SET_SYSTEM_LOGS:
+      return [action.payload.newlog, ...state.reverse()];
+    default:
+      return state.reverse();
+  }
+};
